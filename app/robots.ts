de58a50b7d36ea5a63ec8591cbba98cec5739e48
Radheshyam Dhangar <50437447+radheshyamdhangar@users.noreply.github.com>
2026-09-15 14:07:17 +0530

    web indexing

---
+++ app/robots.ts
@@ -0,0 +1,6 @@
+export default function robots() {
+    return {
+        rules: { userAgent: '*', allow: '/' },
+        sitemap: 'https://www.r18ailabs.com/sitemap.xml',
+    }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://www.r18ailabs.com',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+    ]
+}
